bbfecb30321bb61a4db55cc10224242a013bdb43
Jay Mingle <jkawudi@gmail.com>
2024-02-21 06:23:06 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Search from "./components/search/Search.jsx";
 
 function App() {
 
+    const handleSearchChanges = searchValue => {
+        console.log(searchValue);
+    }
+
   return (
     <div className="container">
         <Search onSearchChange={handleSearchChanges}/>
