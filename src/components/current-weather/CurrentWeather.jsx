2d1bdc79bce00d10c93b446a432640663672b3d8
Jay Mingle <jkawudi@gmail.com>
2024-02-26 08:26:23 +0000

    Changes

--- src/components/current-weather/CurrentWeather.jsx
+++ src/components/current-weather/CurrentWeather.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './CurrentWeather.css'
 
 const CurrentWeather = () => {
     return (
         <div>
-
+            <p>Hello World</p>
         </div>
     );
 };
