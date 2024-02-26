1cc292025ee6fa4fe1d8a7a0ad5f07e95d652ca3
Jay Mingle <jkawudi@gmail.com>
2024-02-26 10:58:43 +0000

    Changes

--- src/components/current-weather/CurrentWeather.jsx
+++ src/components/current-weather/CurrentWeather.jsx
@@ -5,10 +5,15 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
            <div className="top">
-               <p className="city">Belgrade</p>
-               <p className="weather-description">Sunny</p>
+               <div>
+                   <p className="city">Belgrade</p>
+                   <p className="weather-description">Sunny</p>
+               </div>
+               <img src="icons/01d.png" alt="weather" className="weather-icon"/>
            </div>
-            <img src="icons/01d.png" alt="weather" className="weather-icon"/>
+            <div className="bottom">
+                
+            </div>
         </div>
     );
 };
