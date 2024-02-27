b3834f9326b05c8c25cc11ac98ab16bc6a85e781
Jay Mingle <jkawudi@gmail.com>
2024-02-27 02:42:28 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import {useState} from "react";
 import './App.css'
 import Search from "./components/search/Search.jsx";
 import CurrentWeather from "./components/current-weather/CurrentWeather.jsx";
-// import {WEATHER_API_URL, WEATHER_API_KEY} from "./api";
+import {WEATHER_API_URL, WEATHER_API_KEY} from "./api";
 
 function App() {
-        // const [currentWeather, setCurrentWeather] = useState(null)
-        // const [forecastWeather, setForecastWeather] = useState(null)
+        const [currentWeather, setCurrentWeather] = useState(null)
+        const [forecastWeather, setForecastWeather] = useState(null)
 
 
     const handleSearchChanges = searchValue => {
@@ -16,22 +16,22 @@ function App() {
         console.log('Longitude: ',longitude)
 
 
-        // const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
-        // const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
-        //
-        // Promise.all([currentWeatherFetch, forecastWeatherFetch])
-        //     .then(async (response) => {
-        //         const currentResponse = await response[0].json();
-        //         const forecastResponse = await response[1].json();
-        //
-        //         setCurrentWeather({city: searchValue.label ,...currentResponse})
-        //         setForecastWeather({city: searchValue.label, ...forecastResponse})
-        //     })
-        //     .then(error => console.log(error))
+        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+        const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+
+        Promise.all([currentWeatherFetch, forecastWeatherFetch])
+            .then(async (response) => {
+                const currentResponse = await response[0].json();
+                const forecastResponse = await response[1].json();
+
+                setCurrentWeather({city: searchValue.label ,...currentResponse})
+                setForecastWeather({city: searchValue.label, ...forecastResponse})
+            })
+            .then(error => console.log(error))
     }
 
-    // console.log(currentWeather)
-    // console.log(forecastWeather)
+    console.log(currentWeather)
+    console.log(forecastWeather)
 
   return (
     <div className="container">
