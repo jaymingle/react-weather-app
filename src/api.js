1f501e680ee112bb509eb2142f8530ac547d8e62
Jay Mingle <jkawudi@gmail.com>
2024-02-27 02:41:01 +0000

    Changes

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const GEO_API_URL= 'https://wft-geo-db.p.rapidapi.com/v1/geo ';
+export const GEO_API_URL= 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions= {
     method: 'GET',
     headers: {
@@ -7,5 +7,17 @@ export const geoApiOptions= {
     }
 };
 
-// export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-// export const WEATHER_API_KEY = "b3b64bbc0f4bdd25b474684b5228f488"
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = "b3b64bbc0f4bdd25b474684b5228f488"
+
+// export const geoApiOptions = {
+//     method: "GET",
+//     headers: {
+//         "X-RapidAPI-Key": "35dd0f30bfmshda5176cb8edb13bp11c2c5jsne40364b929d9",// enter your rapid api key here
+//         "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+//     },
+// };
+// export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+//
+// export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+// export const WEATHER_API_KEY = "11d56209e8072b4e4e57319e508e9cff"; // enter your key from openweather API
