948e4b2232d9a51e2592fa2b4a8fa0c02b258aa7
Jay Mingle <jkawudi@gmail.com>
2024-02-27 02:25:55 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {WEATHER_API_URL, WEATHER_API_KEY} from "./api";
 
 function App() {
         const [currentWeather, setCurrentWeather] = useState(null)
-        const [foreastWeather, setForecastWeather] = useState(null)
+        const [forecastWeather, setForecastWeather] = useState(null)
 
 
     const handleSearchChanges = searchValue => {
@@ -16,22 +16,22 @@ function App() {
         console.log('Longitude: ',longitude)
 
 
-        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
-        const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
-
-        Promise.all([currentWeatherFetch, forecastWeatherFetch])
-            .then(async (response) => {
-                const currentResponse = await response[0].json();
-                const forecastResponse = await response[1].json();
-
-                setCurrentWeather({city: searchValue.label ,...currentResponse})
-                setForecastWeather({city: searchValue.label, ...forecastResponse})
-            })
-            .then(error => console.log(error))
+        // const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+        // const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
+        //
+        // Promise.all([currentWeatherFetch, forecastWeatherFetch])
+        //     .then(async (response) => {
+        //         const currentResponse = await response[0].json();
+        //         const forecastResponse = await response[1].json();
+        //
+        //         setCurrentWeather({city: searchValue.label ,...currentResponse})
+        //         setForecastWeather({city: searchValue.label, ...forecastResponse})
+        //     })
+        //     .then(error => console.log(error))
     }
 
-    console.log(currentWeather)
-    console.log(forecastWeather)
+    // console.log(currentWeather)
+    // console.log(forecastWeather)
 
   return (
     <div className="container">
