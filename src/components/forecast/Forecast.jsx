9ef71a27706484366cf856feb2e9e613b0ce49f0
Jay Mingle <jkawudi@gmail.com>
2024-02-27 05:28:29 +0000

    Changes

--- src/components/forecast/Forecast.jsx
+++ src/components/forecast/Forecast.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Accordion} from "react-accessible-accordion";
+import {Accordion, AccordionItemHeading, AccordionItem, AccordionItemPanel} from "react-accessible-accordion";
 
-const Forecast = () => {
+const Forecast = ({data}) => {
     return (
         <div>
             <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading></AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
 
+                ))}
             </Accordion>
         </div>
     );
